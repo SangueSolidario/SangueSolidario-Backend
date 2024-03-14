83fdd0592bb6106bc415e64e63edaf46f04d5c73
ttiagojm <tiagodeha@protonmail.com>
2024-03-14 18:03:51 +0000

    Automated GithubActions and starting APIM add API

--- automate.ps1
+++ automate.ps1
@@ -1,3 +1,4 @@
+$do_github=$args[0]
 $resource_name="apibackend"
 $apim_name="apimbackend"
 $apim_id="apisanguesolidario"
@@ -19,6 +20,9 @@ az apim show --name $apim_name --resource-group $resource_name
 
 # Criar API Management
 if($LASTEXITCODE -ne 0){
+    # Elimina APIM caso tenha sido soft-deleted no passado
+    az apim deletedservice purge --location $location --service-name $apim_name
+
     echo "A criar o API Manegement $apim_name"
     az apim create --name $apim_name --resource-group $resource_name --location $location --publisher-email $email --publisher-name $organization
 }
@@ -46,9 +50,19 @@ az webapp show --name $webapp --resource-group $resource_name
 # Criar WebApp
 if($LASTEXITCODE -ne 0){
     echo "A criar o WebApp $webapp"
-    az webapp create --name $webapp --resource-group $resource_name --plan $servico_app
+    az webapp create --name $webapp --resource-group $resource_name --plan $servico_app --runtime "NODE:20-lts"
 }
 
 # Configurar Github Actions
-$github_repo="ttiagojm/SangueSolidarioBack"
-az webapp deployment github-actions add --repo $github_repo -g $resource_name -n $webapp -b main --login-with-github
+if($do_github){
+    $github_repo="ttiagojm/SangueSolidarioBack"
+    az webapp deployment github-actions add --repo $github_repo -g $resource_name -n $webapp -b main --login-with-github
+} else{
+    echo "Adicionar Github Actions ignorado"
+}
+
+# Atribuir AppService à API openAPI
+$api_name="sanguesolidarioapi"
+$app_service_url="https://${webapp}.azurewebsites.net"
+$openapi_url="${app_service_url}/api"
+az apim api import --api-id $api_name -g $resource_name --display-name $api_name --path "/api" -n $apim_name --specification-format Swagger --specification-url $openapi_url
