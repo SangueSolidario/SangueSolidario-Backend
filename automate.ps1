82587ff37043ad28712227261630c2f9358b006c
ttiagojm <tiagodeha@protonmail.com>
2024-05-21 19:05:42 +0100

    New trigger function and fixed openAPI

--- automate.ps1
+++ automate.ps1
@@ -1,11 +1,14 @@
-# Flag (-github) para definir se quere-se conectar, pele primeira vez, ao Github Actions
 param(
     [Parameter(HelpMessage="Criar Github Actions")]
     [switch]$github = $False,
 
     [Parameter(HelpMessage="Criar variáveis de ambiente")]
-    [switch]$createVars = $False
+    [switch]$createVars = $False,
+
+    [Parameter(HelpMessage="Gerar email e serviço de comunicação")]
+    [switch]$genEmail = $False
 )
+
 $resource_name="apibackend"
 $apim_name="sanguesolidario"
 $apim_id="apisanguesolidario"
@@ -15,7 +18,7 @@ $organization="SangueSolidario"
 $webapp="webapp-sanguesolidario"
 
 # Criar grupo de recursos
-if( (az group exists --name $resource_name) -eq $false){
+if ((az group exists --name $resource_name) -eq $false) {
     echo "A criar o grupo de recursos $resource_name"
     az group create --location $location --name $resource_name
 }
@@ -27,7 +30,7 @@ az group wait --exists --resource-group $resource_name
 az apim show --name $apim_name --resource-group $resource_name
 
 # Criar API Management
-if($LASTEXITCODE -ne 0){
+if ($LASTEXITCODE -ne 0) {
     # Elimina APIM caso tenha sido soft-deleted no passado
     az apim deletedservice purge --location $location --service-name $apim_name
 
@@ -38,14 +41,13 @@ if($LASTEXITCODE -ne 0){
 echo "Á espera que o API Management seja criado"
 az apim wait --exists --resource-group $resource_name --name $apim_name
 
-
 $servico_app="servicoweb-sanguesolidario"
 
 # Se der erro o $LASTEXITCODE não será 0
 az webapp show --name $servico_app --resource-group $resource_name
 
 # Criar API Service
-if($LASTEXITCODE -ne 0){
+if ($LASTEXITCODE -ne 0) {
     echo "A criar o App Service $servico_app"
     az appservice plan create --name $servico_app --resource-group $resource_name --sku B1 --is-linux
 }
@@ -54,19 +56,19 @@ if($LASTEXITCODE -ne 0){
 az webapp show --name $webapp --resource-group $resource_name
 
 # Criar WebApp
-if($LASTEXITCODE -ne 0){
+if ($LASTEXITCODE -ne 0) {
     echo "A criar o WebApp $webapp"
     az webapp create --name $webapp --resource-group $resource_name --plan $servico_app --runtime "NODE:20-lts"
 }
 
 # Configurar Github Actions
-if($github){
+if ($github) {
     # Esperar 30 segundos pela webapp
     echo "A dormir por 30 segundos"
-    Start-Sleep -Milliseconds 30000
-    $github_repo="SangueSolidario/SangueSolidario-Backend"
+    Start-Sleep -Seconds 30
+    $github_repo="ttiagojm/SangueSolidario-Backend"
     az webapp deployment github-actions add --repo $github_repo -g $resource_name -n $webapp -b main --login-with-github
-} else{
+} else {
     echo "Adicionar Github Actions ignorado"
 }
 
@@ -76,7 +78,7 @@ $openapi_url="${app_service_url}/api/swagger.json"
 
 # Esperar 30 segundos
 echo "A dormir por 30 segundos"
-Start-Sleep -Milliseconds 30000
+Start-Sleep -Seconds 30
 
 # Atribuir AppService à API openAPI
 $api_name="sanguesolidarioapi"
@@ -108,21 +110,88 @@ az cosmosdb sql container create --account-name $cosmos_name --database-name $db
 
 # Obter a chave primária
 $primaryKey = az cosmosdb keys list --name $cosmos_name --resource-group $resource_name --type keys --output json --query primaryMasterKey -o tsv
+
 # Obter .env e trocar o valor da KEY pela nova KEY criada
 $content = Get-Content ".env"
-$new_content = $content -replace 'AUTH_KEY=.*', "AUTH_KEY=$primaryKey"
+$new_content = $content -replace 'COSMOSKEY=.*', "COSMOSKEY=$primaryKey"
 $new_content = $new_content -replace 'DB=.*', "DB=$db_name"
 $new_content | Set-Content ".env"
 
+# Criar Blob Storage Account
+$blobStorageAccount = "myblobstorage20210941"
+az storage account create --name $blobStorageAccount --location $location --resource-group $resource_name --sku Standard_LRS --kind StorageV2 --access-tier hot --allow-blob-public-access true
+
+$blobStorageAccountKey = az storage account keys list -g $resource_name -n $blobStorageAccount --query "[0].value" -o tsv
+
+az storage container create --name images --account-name $blobStorageAccount --account-key $blobStorageAccountKey
+
+az webapp config appsettings set --name $webapp --resource-group $resource_name `
+  --settings `
+    "AzureStorageConfig__AccountName=$blobStorageAccount" `
+    "AzureStorageConfig__ImageContainer=images" `
+    "AzureStorageConfig__AccountKey=$blobStorageAccountKey"
+
+# Reiniciar webapp para garantir que as variáveis ficam definidas
+az webapp restart --name $webapp --resource-group $resource_name
+
+# Atualizar valores .env com novas informações dos recursos do Azure
+$content = Get-Content ".env"
+$new_content = $content -replace 'COSMOSKEY=.*', "COSMOSKEY=$primaryKey"
+$new_content = $new_content -replace 'DB=.*', "DB=$db_name"
+$new_content = $new_content -replace 'BLOB_KEY=.*', "BLOB_KEY=$blobStorageAccountKey"
+$new_content = $new_content -replace 'BLOB=.*', "BLOB=$blobStorageAccount"
+$new_content | Set-Content ".env"
+
 # Set KEY=VALUE do .env nas variáveis de ambiente da webapp
-if($createVars){
+if ($createVars) {
     echo "A criar variáveis de ambiente"
-    Get-Content .env | ForEach-Object {
+    Get-Content ".env" | ForEach-Object {
         $pair = $_ -split "="
         $settingName = $pair[0].Trim()
         $settingValue = $pair[1].Trim()
         az webapp config appsettings set --name $webapp --resource-group $resource_name --settings "$settingName=$settingValue"
     }
 }
-# Reinicar webapp para garantir que as variáveis ficam definidas
-az webapp restart --name $webapp --resource-group $resource_name
+
+# Criar Communication Service e Email Service se $genEmail for true
+$commService="comm20210941"
+$emailService="emailService20210941"
+
+if ($genEmail) {
+    # Criar serviço de comunicação
+    az communication create --data-location unitedstates --name $commService -g $resource_name --location global
+
+    # Criar serviço de email
+    az communication email create -n $emailService -g $resource_name --location global --data-location unitedstates
+
+    # Criar domínio de email Azure
+    az communication email domain create --domain-name AzureManagedDomain --email-service-name $emailService -g $resource_name --location global --domain-management AzureManaged
+
+    # É necessário ir ao portal e conectar o domínio ao serviço de comunicação
+    Read-Host -Prompt "Ir ao Portal Azure -> Communication Service -> Connect your email domains -> Connect Domain -> Selecionar todos os serviços criados... Pressione ENTER para continuar"
+
+    # Obter string de conexão para o serviço de comunicação
+    $commServiceConn = az communication list-key --name $commService --resource-group $resource_name --query "primaryConnectionString" -o tsv
+
+    $content = Get-Content ".env"
+    $new_content = $content -replace 'EMAILCONN=.*', "EMAILCONN=$commServiceConn"
+    $new_content | Set-Content ".env"
+}
+
+# Criar Function App
+$functionAppName="detectNewCampanhas"
+az functionapp create -g $resource_name --consumption-plan-location $location --runtime node --functions-version 4 --name $functionAppName --storage-account $blobStorageAccount
+
+# Passar a string de conexão para o CosmosDB
+az functionapp config appsettings set --name $functionAppName -g $resource_name `
+ --settings `
+ "COSMOS_HOST=https://$cosmos_name.documents.azure.com:443/" `
+ "COSMOS_KEY=$primaryKey" `
+ "EMAIL_CONN=$(az communication list-key --name $commService --resource-group $resource_name --query 'primaryConnectionString' -o tsv)" `
+ "SENDER_EMAIL=$(az communication email domain list --email-service-name $emailService -g $resource_name --query '[0].fromSenderDomain' -o tsv)" `
+ "BLOB=$blobStorageAccount" `
+ "BLOB_KEY=$blobStorageAccountKey"
+
+# Publish Function APP Trigger
+cd src/email/
+func azure functionapp publish detectNewCampanha
